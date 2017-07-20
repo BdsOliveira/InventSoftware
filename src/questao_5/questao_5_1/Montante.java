00ecb748e491ca68dde82ac17c61ee177e38c043
Bruno <Bruno@Bruno-PC>
2017-07-20 16:41:15 -0300

    Classes com suas respectivas resoluções.

--- src/questao_5/questao_5_1/Montante.java
+++ src/questao_5/questao_5_1/Montante.java
@@ -3,8 +3,8 @@ package questao_5.questao_5_1;
 public class Montante {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Teste");
 		
 	}
 
