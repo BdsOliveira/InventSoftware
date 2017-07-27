578a483874f8c3b142758c620855df6ca05d8abf
Bruno <Bruno@Bruno-PC>
2017-07-27 16:09:47 -0300

    Classes com suas respectivas resoluções.

--- src/questao_5/Montante.java
+++ src/questao_5/Montante.java
@@ -2,11 +2,33 @@ package questao_5;
 
 public class Montante {
 
-	public float obtenhaMontante(float capitalInicial, float taxaJuro, float prazo) {
-		return 0;
+	// Resolução da questão 5.1
+	static double obtenhaMontante(double capitalInicial, double taxaJuro, double prazo) {
+		double juros = 1;
+		for (int i = 0; i < prazo; i++) {
+			juros = juros * (1 + taxaJuro);
+		}
+		return capitalInicial * juros; 
+	}
+	
+	// Resolução da questão 5.1
+	static double obtenhaJuroToral(double montante, double capitalInicial) {
+		return montante - capitalInicial;
 	}
 	
 	public static void main(String[] args) {
+		
+		double capitalInicial, taxaJuro, prazo;
+		
+		capitalInicial = 10000;
+		taxaJuro = 3.0 / 100;
+		prazo = 3;
+		
+		double montante = obtenhaMontante(capitalInicial, taxaJuro, prazo);
+		double juroTotal = obtenhaJuroToral(montante, capitalInicial);
+		
+		System.out.println("Questão 5.2:\n\tMontante: " + montante);
+		System.out.println("Questão 5.4:\n\tJuro Total: " + juroTotal);
 		
 	}
 
